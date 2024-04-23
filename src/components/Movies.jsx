10a64d332723a1f0670a04286c73e1ab392b010c
Milan Wesołowski <milanweso@gmail.com>
2024-04-23 16:57:19 +0200

    Implement nice styles

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -3,15 +3,19 @@ import { useState, useEffect } from "react";
 function Movies() {
     const [movie, setMovie] = useState(null);
     const [year, setYear] = useState(null);
+    const [director, setDirector] = useState(null);
 
     useEffect(() => {
         const fetchMovie = async () => {
             try {
-                const response = await fetch('https://api.themoviedb.org/3/movie/873?api_key=7f0e9b5e25babb2fe0d751bf7e14f1f0');
-                // id: 873, 558915
+                const response = await fetch('https://api.themoviedb.org/3/movie/873?api_key=7f0e9b5e25babb2fe0d751bf7e14f1f0&append_to_response=credits'); // id: 873, 558915
                 const data = await response.json();
+
                 setMovie(data);
                 setYear(`(${data.release_date.slice(0, 4)})`);
+                setDirector(data.credits.crew.filter(({ job }) => job === 'Director').map(({ name }) => name).join(', '));
+
+                console.log(data.credits.crew.filter(({ job }) => job === 'Director').map(({ name }) => name));
             } catch (error) {
                 console.error('Error fetching data: ', error);
             }
@@ -21,17 +25,21 @@ function Movies() {
     }, [])
 
     return (
-        <div>
+        <>
             {movie ? (
-                <div>
-                    <h1>{movie.title} {year}</h1>
-                    <p>{movie.overview}</p>
-                    <img src={`https://image.tmdb.org/t/p/w400/${movie.poster_path}`} alt={`${movie.title} ${year}`} /> {/* width 400, normal: original */}
+                <div className="movie">
+                    <img src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} alt={`${movie.title} ${year}`} />
+
+                    <div className="info">
+                        <h1>{movie.title} {year}</h1>
+                        <h2>Directed by {director}</h2>
+                        <p>{movie.overview}</p>
+                    </div>
                 </div>
             ) : (
-                <p>Loading...</p>
+                <h1>Loading...</h1>
             )}
-        </div>
+        </>
     );
 }
 
