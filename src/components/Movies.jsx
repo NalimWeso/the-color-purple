5b4a8d87f79e271a6a564f9fe26e497a018ecd3a
Milan Wesołowski <milanweso@gmail.com>
2024-04-24 10:18:07 +0200

    Correct elements

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -12,10 +12,8 @@ function Movies() {
                 const data = await response.json();
 
                 setMovie(data);
-                setYear(`(${data.release_date.slice(0, 4)})`);
+                setYear(data.release_date.slice(0, 4));
                 setDirector(data.credits.crew.filter(({ job }) => job === 'Director').map(({ name }) => name).join(', '));
-
-                console.log(data.credits.crew.filter(({ job }) => job === 'Director').map(({ name }) => name));
             } catch (error) {
                 console.error('Error fetching data: ', error);
             }
@@ -28,16 +26,18 @@ function Movies() {
         <>
             {movie ? (
                 <div className="movie">
-                    <img src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} alt={`${movie.title} ${year}`} />
+                    <img src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} alt={`${movie.title} (${year})`} />
 
                     <div className="info">
-                        <h1>{movie.title} {year}</h1>
+                        <h1>{year} Version</h1>
                         <h2>Directed by {director}</h2>
                         <p>{movie.overview}</p>
                     </div>
                 </div>
             ) : (
-                <h1>Loading...</h1>
+                <div className="movie">
+                    <h1 className="loading">Loading...</h1>
+                </div>
             )}
         </>
     );
