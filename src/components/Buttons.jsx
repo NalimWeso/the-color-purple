28448bf0f872d67af370d2744b4ae3560d97b048
Milan Wesołowski <milanweso@gmail.com>
2024-05-02 18:10:47 +0200

    Implement clickButton

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,10 +1,20 @@
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 
 function Buttons({ setMovieId }) {
+    const [classOne, setClassOne] = useState('clicked');
+    const [classTwo, setClassTwo] = useState(null);
+
+    function clickButton(num, btn) {
+        setMovieId(prev => prev !== num ? num : prev);
+        setClassOne(btn === 1 ? 'clicked' : null);
+        setClassTwo(btn === 2 ? 'clicked' : null);
+    }
+
     return (
         <div className="buttons">
-            <button onClick={() => setMovieId(873)} className="clicked">1985</button>
-            <button onClick={() => setMovieId(558915)}>2023</button>
+            <button onClick={() => clickButton(873, 1)} className={classOne}>1985</button>
+            <button onClick={() => clickButton(558915, 2)} className={classTwo}>2023</button>
         </div>
     );
 }
