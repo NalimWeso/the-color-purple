41aa381e4a62050c7acc6ad713be465ef64405ba
Milan Wesołowski <milanweso@gmail.com>
2024-05-02 22:04:34 +0200

    Implement PostCSS

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,15 @@
-/* eslint-disable no-unused-vars */
+import postcssImport from 'postcss-import';
+import precss from 'precss';
 import autoprefixer from 'autoprefixer';
+import presetEnv from 'postcss-preset-env';
+import cssnano from 'cssnano';
 
 export default {
-    plugins: {
-        autoprefixer: {}
-    }
+    plugins: [
+        postcssImport,
+        precss,
+        autoprefixer,
+        presetEnv({ stage: 1 }),
+        cssnano({ preset: 'default' })
+    ]
 };
